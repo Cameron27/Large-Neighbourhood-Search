26e148c88a309cafce8e2336c729d68f3f156481
Cameron Salisbury <cam2772@gmail.com>
2020-07-23 11:35:36 +1200

    Environment setup

---
+++ src/main/java/org/compx556/RectanglePacker.java
@@ -0,0 +1,7 @@
+package org.compx556;
+
+public class RectanglePacker {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+    }
+}
---
+++ src/test/java/org/compx556/Test.java
@@ -0,0 +1,8 @@
+package org.compx556;
+
+public class Test {
+    @org.junit.Test
+    public void main() {
+        RectanglePacker.main(new String[]{});
+    }
+}
