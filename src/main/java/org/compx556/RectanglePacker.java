989c17de84e7824aa979947f7e21cc5b3ac21360
maravis <sivaramigcse@gmail.com>
2020-07-29 00:13:28 +1200

    Created crude version of a potential evaluator

--- src/main/java/org/compx556/RectanglePacker.java
+++ src/main/java/org/compx556/RectanglePacker.java
@@ -2,6 +2,14 @@ package org.compx556;
 
 public class RectanglePacker {
     public static void main(String[] args) {
-        System.out.println("Hello world ");
+        Box a = new Box(5,10,0);
+        Box b = new Box(10,15,10);
+        BoxList c = new BoxList();
+        c.addBox(a);
+        c.addBox(b);
+    }
+
+    public int evaluator(BoxList solution){
+        return solution.calculateHeight();
     }
 }
