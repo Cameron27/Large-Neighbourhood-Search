f5701351ca417437218c43651d659408c7c138fc
Cameron Salisbury <cam2772@gmail.com>
2020-07-29 16:35:36 +1200

    Create global random class

---
+++ src/main/java/org/compx556/util/GlobalRandom.java
@@ -0,0 +1,19 @@
+package org.compx556.util;
+
+import java.util.Random;
+
+public class GlobalRandom {
+    private static Random rnd = new Random();
+
+    public static boolean nextBoolean() {
+        return rnd.nextBoolean();
+    }
+
+    public static int nextInt() {
+        return rnd.nextInt();
+    }
+
+    public static int nextInt(int bound) {
+        return rnd.nextInt(bound);
+    }
+}
