0ac8ddd7b72c72b76a327799175022f4a2106a52
Cameron Salisbury <cam2772@gmail.com>
2020-07-30 18:56:32 +1200

    Added cli parameter support

--- src/main/java/org/compx556/Config.java
+++ src/main/java/org/compx556/Config.java
@@ -1,15 +1,29 @@
 package org.compx556;
 
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.converters.FileConverter;
 import org.compx556.function.DestructionFunction;
 import org.compx556.function.InitialisationFunction;
 import org.compx556.function.RepairFunction;
 
+import java.io.File;
+
 public class Config {
     public InitialisationFunction initialisationFunction;
-
     public DestructionFunction destructionFunction;
-
     public RepairFunction repairFunction;
+
+    @Parameter(description = "data", required = true, converter = FileConverter.class)
+    public File dataFile;
+
+    @Parameter(names = {"-o", "-output"}, description = "Name of file to save result image as", converter = FileConverter.class)
+    public File outFile;
+
+    @Parameter(names = {"-s", "-seed"}, description = "Seed to use for randomness")
+    public Integer seed;
+
+    @Parameter(names = {"-h", "-help"}, help = true, hidden = true)
+    public boolean help;
 
     public Config(InitialisationFunction initialisationFunction, DestructionFunction destructionFunction, RepairFunction repairFunction) {
         this.initialisationFunction = initialisationFunction;
--- src/main/java/org/compx556/RectanglePacker.java
+++ src/main/java/org/compx556/RectanglePacker.java
@@ -1,13 +1,28 @@
 package org.compx556;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+import org.compx556.function.DestructionFunction;
+import org.compx556.function.InitialisationFunction;
+import org.compx556.function.RepairFunction;
+
 public class RectanglePacker {
     public static void main(String[] args) {
-        Box a = new Box(5, 10, 0);
-        Box b = new Box(10, 15, 10);
-        BoxList c = new BoxList(400);
-        c.add(a);
-        c.add(b);
+        Config config = new Config(InitialisationFunction.random, DestructionFunction.randomNRemove, RepairFunction.randomLocationOptimumX);
 
-        System.out.println(c.toString());
+        JCommander builder = JCommander.newBuilder()
+                .addObject(config)
+                .build();
+        try {
+            builder.parse(args);
+        } catch (ParameterException e) {
+            System.out.println(e.getMessage());
+            e.usage();
+            return;
+        }
+        if (config.help) {
+            builder.usage();
+            return;
+        }
     }
 }
