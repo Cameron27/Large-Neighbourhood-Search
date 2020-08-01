9e08eaf18a709b98b11be31ee35481f1aefcd2c5
maravis <sivaramigcse@gmail.com>
2020-08-02 01:17:13 +1200

    Program can now read in CSV files, TODO: determine Xlocation, determine filepath and store width of object

--- src/main/java/org/compx556/RectanglePacker.java
+++ src/main/java/org/compx556/RectanglePacker.java
@@ -1,13 +1,54 @@
 package org.compx556;
 
-public class RectanglePacker {
-    public static void main(String[] args) {
-        Box a = new Box(5, 10, 0);
-        Box b = new Box(10, 15, 10);
-        BoxList c = new BoxList(400);
-        c.add(a);
-        c.add(b);
+import java.io.*;
+import java.util.Random;
 
-        System.out.println(c.toString());
+public class RectanglePacker {
+
+    public static void main(String[] args) throws IOException {
+
+        BoxList initialList = new BoxList(400);
+        File file  = new File("C:\\Users\\sivar\\Downloads\\COMPX556\\COMPX556Assignment1\\testData.csv");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        String line = br.readLine();
+        boolean isBoxData = false;
+        int size = 0;
+
+        Random rand = new Random();
+
+        while (line != null) {
+            String[] data = line.split(",");
+            if (isBoxData){
+                int numBox = Integer.parseInt(data[0]);
+                int xLocation = rand.nextInt(); // Placeholder
+                int width = Integer.parseInt(data[1]);
+                int height = Integer.parseInt(data[2]);
+                Box inputBox = new Box(width,height,xLocation);
+                initialList.add(inputBox);
+
+                if(numBox == size){
+                    isBoxData = false;
+                }
+            }
+            else{
+                if(line.contains("name:")){
+                    String name = data[2];
+                    System.out.println(name);
+                }
+                if(line.contains("width")){
+                    isBoxData = true;
+                }
+                if(line.contains("size")){
+                    size = Integer.parseInt(data[2]);
+                }
+            }
+            line = br.readLine();
+
+        }
+
+        System.out.println(initialList.toString());
     }
+
 }
