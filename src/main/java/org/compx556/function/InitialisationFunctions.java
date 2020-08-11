5de43d069947b8bd4c7f07cbc3292bf0cbcc4bee
Cameron Salisbury <cam2772@gmail.com>
2020-08-11 21:04:23 +1200

    Changed InitialisationFunction to abstract class

--- src/main/java/org/compx556/function/InitialisationFunctions.java
+++ src/main/java/org/compx556/function/InitialisationFunctions.java
@@ -11,23 +11,31 @@ public class InitialisationFunctions {
      * Randomises the order of the <code>Box</code> objects and sets every <code>Box</code> to have a random x value and
      * rotation.
      */
-    public static final InitialisationFunction random = list -> {
-        // create new list with random order
-        BoxList output = list.clone();
-        Collections.shuffle(output);
-
-        // rotate boxes randomly
-        for (int i = 0; i < output.size(); i++) {
-            Box b = list.get(i).clone();
-            if (GlobalRandom.nextBoolean()) {
-                // clone box if it is going to be rotated
-                b.rotate();
-            }
-            b.setXStart(GlobalRandom.nextInt(list.getObjectSize() - b.getWidth() + 1));
-
-            output.set(i, b);
+    public static final InitialisationFunction random = new InitialisationFunction() {
+        @Override
+        public String getName() {
+            return "Random";
         }
 
-        return output;
+        @Override
+        public BoxList apply(BoxList solution) {
+            // create new list with random order
+            BoxList output = solution.clone();
+            Collections.shuffle(output);
+
+            // rotate boxes randomly
+            for (int i = 0; i < output.size(); i++) {
+                Box b = solution.get(i).clone();
+                if (GlobalRandom.nextBoolean()) {
+                    // clone box if it is going to be rotated
+                    b.rotate();
+                }
+                b.setXStart(GlobalRandom.nextInt(solution.getObjectSize() - b.getWidth() + 1));
+
+                output.set(i, b);
+            }
+
+            return output;
+        }
     };
 }
