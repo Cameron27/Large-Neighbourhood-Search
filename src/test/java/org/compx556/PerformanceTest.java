0b8dc020fb1934b7d76a6417976b4ba225647cb7
Cameron Salisbury <cam2772@gmail.com>
2020-08-17 15:34:40 +1200

    Added final performance test

--- src/test/java/org/compx556/PerformanceTest.java
+++ src/test/java/org/compx556/PerformanceTest.java
@@ -1,8 +1,42 @@
 package org.compx556;
 
+import org.compx556.util.GlobalRandom;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.zip.DataFormatException;
 
 public class PerformanceTest {
+    /**
+     * Test the performance of the final algorithm on all datasets.
+     */
+    @Test
+    public void NValueOptimisationTest() throws IOException, DataFormatException {
+        // datasets to test on
+        String[] testFiles = new String[]{"/m1a.csv", "/m2c.csv", "/m3d.csv"};
+        // runtime for each dataset
+        int[] runtimes = new int[]{60000, 90000, 120000};
+
+        // for each file
+        for (int i = 0; i < testFiles.length; i++) {
+            String testFile = testFiles[i];
+
+            // set seed
+            GlobalRandom.setSeed((long) (testFile.hashCode()));
+
+            // set config
+            Config config = RectanglePacker.defaultConfig.clone();
+            config.dataFile = new File(getClass().getResource(testFile).getFile());
+            config.runtime = runtimes[i];
+
+            // run tests
+            RectanglePacker packer = new RectanglePacker(config);
+            PerformanceTest.runTest(packer, testFile);
+        }
+    }
+
     public static void runTest(RectanglePacker rectanglePacker, String name) {
         int count = 10;
         int[] results = new int[count];
